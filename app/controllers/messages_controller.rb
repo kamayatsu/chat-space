248575cad3b2718c901c5b3c49d9164a1dce8111
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-06-03 01:37:42 +0900

    update member

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group , :set_member
+  before_action :set_group
 
   def index
     @message = Message.new
@@ -25,12 +25,5 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-  end
-
-  def set_member
-    @name = []
-    @group.users.each do |user|
-      @name << user.name
-    end
   end
 end
