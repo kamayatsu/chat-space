54df591134d9035d18f3da92a05d2e2afc5afaa6
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-06-16 21:29:26 +0900

    install rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
